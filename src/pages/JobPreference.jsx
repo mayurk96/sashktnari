723cc3be5ce0a6f32db34bb88560e65175159b4e
mayurk96 <173041171+mayurk96@users.noreply.github.com>
2025-05-29 23:42:04 +0530

    Update JobPreference.jsx

--- src/pages/JobPreference.jsx
+++ src/pages/JobPreference.jsx
@@ -395,25 +395,7 @@ const JobPreference = () => {
   );
 };
 
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
-});
 
 export default JobPreference;
 
