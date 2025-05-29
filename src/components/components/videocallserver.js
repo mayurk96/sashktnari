75b0db180b22a7ac6a404bef4e74bfc3570c20f0
mayurk96 <173041171+mayurk96@users.noreply.github.com>
2025-05-29 22:44:31 +0530

    Update videocallserver.js

--- src/components/components/videocallserver.js
+++ src/components/components/videocallserver.js
@@ -23,6 +23,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log('Server is running on port 5000');
+// ... existing code ...
+server.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running on Render or port 5000');
 });
+// ... existing code ...
