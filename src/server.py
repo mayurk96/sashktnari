0d9d4f74a1c626978f4de6ef7c5c09c89d1ef6e8
mayurk96 <173041171+mayurk96@users.noreply.github.com>
2025-05-29 23:06:01 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -196,4 +196,6 @@ def get_recommendations():
         }), 500
 
 if __name__ == '__main__':
-    app.run(port=5002, debug=True) 
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
