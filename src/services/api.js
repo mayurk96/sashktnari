6a0f73102ac959aea752ade3ceec1e3a2f5084b4
mayurk96 <173041171+mayurk96@users.noreply.github.com>
2025-05-29 17:19:55 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://sashktnari-3t9m.onrender.com/api';
+const API_URL = 'https://sashktnari-3t9m.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
